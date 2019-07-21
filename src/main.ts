1d67b78d4acc1e282d37942646c17ac604783a77
Daniel Wu <wuon@protonmail.com>
2019-07-20 23:31:48 -0700

    added spinner

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -9,6 +10,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './scss/styles.scss';
+import 'vue-loading-overlay/dist/vue-loading.css';
 
 library.add(faArrowRight);
 
@@ -17,6 +19,7 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
+Vue.use(Loading);
 
 new Vue({
   router,
--- src/views/RSVP.vue
+++ src/views/RSVP.vue
@@ -1,22 +1,8 @@
 <template>
-  <div class="signup">
-    <div class="signup-left-panel">
-      <div class="item">
-        <div class="logo-container">
-          <img class="signup-logo" src="../assets/logo_box.svg" />
-          <h1 class="logo-text">CH</h1>
-        </div>
-        <div class="step-container">
-          <div class="step">
-            <a>Step 1</a>
-          </div>
-          <div class="step">
-            <a>{{ email }}</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="signup-right-panel"></div>
+  <div class="rsvp-container">
+    <div class="rsvp-item">b</div>
+    <div class="rsvp-item">{{message}}</div>
+    <div class="rsvp-item">b</div>
   </div>
 </template>
 
@@ -38,6 +24,14 @@ export default class RSVP extends Vue {
   @Getter("fullName", { namespace }) fullName!: string;
 
   id: string | (string | null)[] = "";
+  message: string = "";
+  status: {
+    isAccepted: boolean;
+    rsvpSubmitted: boolean;
+  } = {
+    isAccepted: false,
+    rsvpSubmitted: false
+  };
 
   mounted() {
     this.id = this.$route.query.id;
@@ -47,14 +41,18 @@ export default class RSVP extends Vue {
   }
 
   processID() {
+    // @ts-ignore
+    const LOADER = this.$loading.show();
     this.axios
       .get(`https://api.citizenhacks.com/application/status/${this.id}`)
       .then(res => {
-        console.log(res);
-        this.initWithId(this.id);
+        LOADER.hide();
+        this.status = res.data;
+        console.log(this.status.isAccepted);
       })
       .catch(e => {
         console.log(e);
+        this.message = "Unfortunate message.";
       });
   }
 
@@ -62,67 +60,23 @@ export default class RSVP extends Vue {
     const user = this.profile && this.profile.user;
     return (user && user.id) || "";
   }
+
+  get isAccepted() {
+    return this.status.isAccepted;
+  }
+
+  get rsvpSubmitted() {
+    return this.status.rsvpSubmitted;
+  }
 }
 </script>
 
 <style scoped>
-.signup {
-  width: 100%;
-  height: 100vh;
+.rsvp-container {
   display: flex;
-  flex-direction: row;
-  background-color: #fafafa;
-}
-.signup-left-panel {
-  height: 100vh;
-  flex-grow: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: #222222;
-  color: #ffffff;
-}
-.signup-right-panel {
-  flex-grow: 3;
-  display: flex;
-  background-color: #fafafa;
-}
-.item {
-  padding: 50px;
-  text-align: left;
-}
-.signup-h1 {
-  font-size: 64px;
-  margin-bottom: 10px;
-}
-.signup-p {
-  margin-top: 20px;
-  font-size: 18px;
-}
-.signup-logo {
-  height: auto;
-  width: 48px;
-}
-.signup-logo-full {
-  height: 96px;
-  width: auto;
-}
-.logo-container {
-  display: flex;
-  flex-direction: row;
   align-items: center;
-}
-.logo-text {
-  margin-left: 16px;
-}
-.signup-signup {
-  font-size: 24px;
-  text-decoration: underline;
-}
-.signup-signup-icon {
-  margin-left: 8px;
-  font-weight: normal;
-  font-size: 16px;
-  margin-bottom: 2px;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 100vh;
 }
 </style>
